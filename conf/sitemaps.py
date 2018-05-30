5e35d18207099fbdaa37d07fbb5691ae83e153ef
eadpearce <e.pearce@tutanota.com>
2018-05-30 15:11:46 +0100

    Update tests, set up landing page template

--- conf/sitemaps.py
+++ conf/sitemaps.py
@@ -1,24 +1,6 @@
 from django.contrib import sitemaps
 from django.core.urlresolvers import reverse
 
-from core import helpers
-import directory_cms_client.constants
-
-
-class SectorLandingPageSitemap(sitemaps.Sitemap):
-    priority = 0.5
-    changefreq = 'daily'
-
-    def items(self):
-        response = helpers.cms_client.list_by_page_type(
-            directory_cms_client.constants.FIND_A_SUPPLIER_INDUSTRY_TYPE
-        )
-        pages = helpers.handle_cms_response(response)['items']
-        return [page['meta']['slug'] for page in pages]
-
-    def location(self, item):
-        return reverse('sector-detail-verbose', kwargs={'slug': item})
-
 
 class StaticViewSitemap(sitemaps.Sitemap):
     priority = 0.5
@@ -27,9 +9,6 @@ class StaticViewSitemap(sitemaps.Sitemap):
     def items(self):
         return [
             'index',
-            'public-company-profiles-list',
-            'sector-list',
-            'subscribe',
         ]
 
     def location(self, item):
--- core/tests/test_views.py
+++ core/tests/test_views.py
@@ -6,27 +6,6 @@ from core.tests import helpers
 
 
 @patch('core.helpers.cms_client.lookup_by_slug')
-def test_landing_page_context(
-    mock_get_landing_page, settings, client, breadcrumbs
-):
-    page = {
-        'title': 'the page',
-        'industries': [{'title': 'good 1'}],
-        'meta': {'languages': ['en-gb']},
-        'breadcrumbs': breadcrumbs,
-    }
-    mock_get_landing_page.return_value = helpers.create_response(
-        status_code=200,
-        json_payload=page
-    )
-
-    response = client.get(reverse('index'))
-
-    assert response.status_code == 200
-    assert response.context_data['page'] == page
-
-
-@patch('core.helpers.cms_client.lookup_by_slug')
 def test_landing_page_not_found(
     mock_get_landing_page, settings, client
 ):
