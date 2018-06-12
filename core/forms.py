e482aac7e6d0cc7b22a95d202274e4546666351e
eadpearce <e.pearce@tutanota.com>
2018-06-12 10:33:05 +0100

    Update contact form

--- core/forms.py
+++ core/forms.py
@@ -1,17 +1,460 @@
-from django.conf import settings
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Fieldset, Field, Submit, HTML, Div
+
+from captcha.fields import ReCaptchaField
+
+from django.forms import Select, Textarea
+from django.db.models.fields import BLANK_CHOICE_DASH
+from django.utils.translation import ugettext as _
 
 from directory_components import forms, fields
+from directory_constants.constants import choices
+
+from directory_validators.common import not_contains_url_or_email
+from directory_validators.company import no_html
+
+Fieldset.template = 'core/custom_fieldset.html'
+Field.template = 'core/custom_field.html'
+
+COUNTRIES_CHOICES = [
+    ("Afghanistan", "Afghanistan"),
+    ("Albania", "Albania"),
+    ("Algeria", "Algeria"),
+    ("American Samoa", "American Samoa"),
+    ("Andorra", "Andorra"),
+    ("Angola", "Angola"),
+    ("Anguilla", "Anguilla"),
+    ("Antigua and Barbuda", "Antigua and Barbuda"),
+    ("Argentina", "Argentina"),
+    ("Armenia", "Armenia"),
+    ("Aruba", "Aruba"),
+    ("Australia", "Australia"),
+    ("Austria", "Austria"),
+    ("Azerbaijan", "Azerbaijan"),
+    ("The Bahamas", "The Bahamas"),
+    ("Bahrain", "Bahrain"),
+    ("Bangladesh", "Bangladesh"),
+    ("Barbados", "Barbados"),
+    ("Belarus", "Belarus"),
+    ("Belgium", "Belgium"),
+    ("Belize", "Belize"),
+    ("Benin", "Benin"),
+    ("Bermuda", "Bermuda"),
+    ("Bhutan", "Bhutan"),
+    ("Bolivia", "Bolivia"),
+    ("Bosnia and Herzegovina", "Bosnia and Herzegovina"),
+    ("Botswana", "Botswana"),
+    ("Brazil", "Brazil"),
+    ("Brunei", "Brunei"),
+    ("Bulgaria", "Bulgaria"),
+    ("Burkina Faso", "Burkina Faso"),
+    ("Burundi", "Burundi"),
+    ("Cambodia", "Cambodia"),
+    ("Cameroon", "Cameroon"),
+    ("Canada", "Canada"),
+    ("Cape Verde", "Cape Verde"),
+    ("Cayman Islands", "Cayman Islands"),
+    ("Central African Republic", "Central African Republic"),
+    ("Chad", "Chad"),
+    ("Chile", "Chile"),
+    ("People 's Republic of China", "People 's Republic of China"),
+    ("Republic of China", "Republic of China"),
+    ("Christmas Island", "Christmas Island"),
+    ("Cocos(Keeling) Islands", "Cocos(Keeling) Islands"),
+    ("Colombia", "Colombia"),
+    ("Comoros", "Comoros"),
+    ("Congo", "Congo"),
+    ("Cook Islands", "Cook Islands"),
+    ("Costa Rica", "Costa Rica"),
+    ("Cote d'Ivoire", "Cote d'Ivoire"),
+    ("Croatia", "Croatia"),
+    ("Cuba", "Cuba"),
+    ("Cyprus", "Cyprus"),
+    ("Czech Republic", "Czech Republic"),
+    ("Denmark", "Denmark"),
+    ("Djibouti", "Djibouti"),
+    ("Dominica", "Dominica"),
+    ("Dominican Republic", "Dominican Republic"),
+    ("Ecuador", "Ecuador"),
+    ("Egypt", "Egypt"),
+    ("El Salvador", "El Salvador"),
+    ("Equatorial Guinea", "Equatorial Guinea"),
+    ("Eritrea", "Eritrea"),
+    ("Estonia", "Estonia"),
+    ("Ethiopia", "Ethiopia"),
+    ("Falkland Islands", "Falkland Islands"),
+    ("Faroe Islands", "Faroe Islands"),
+    ("Fiji", "Fiji"),
+    ("Finland", "Finland"),
+    ("France", "France"),
+    ("French Polynesia", "French Polynesia"),
+    ("Gabon", "Gabon"),
+    ("The Gambia", "The Gambia"),
+    ("Georgia", "Georgia"),
+    ("Germany", "Germany"),
+    ("Ghana", "Ghana"),
+    ("Gibraltar", "Gibraltar"),
+    ("Greece", "Greece"),
+    ("Greenland", "Greenland"),
+    ("Grenada", "Grenada"),
+    ("Guadeloupe", "Guadeloupe"),
+    ("Guam", "Guam"),
+    ("Guatemala", "Guatemala"),
+    ("Guernsey", "Guernsey"),
+    ("Guinea", "Guinea"),
+    ("Guinea - Bissau", "Guinea - Bissau"),
+    ("Guyana", "Guyana"),
+    ("Haiti", "Haiti"),
+    ("Honduras", "Honduras"),
+    ("Hong Kong", "Hong Kong"),
+    ("Hungary", "Hungary"),
+    ("Iceland", "Iceland"),
+    ("India", "India"),
+    ("Indonesia", "Indonesia"),
+    ("Iran", "Iran"),
+    ("Iraq", "Iraq"),
+    ("Ireland", "Ireland"),
+    ("Israel", "Israel"),
+    ("Italy", "Italy"),
+    ("Jamaica", "Jamaica"),
+    ("Japan", "Japan"),
+    ("Jersey", "Jersey"),
+    ("Jordan", "Jordan"),
+    ("Kazakhstan", "Kazakhstan"),
+    ("Kenya", "Kenya"),
+    ("Kiribati", "Kiribati"),
+    ("North Korea", "North Korea"),
+    ("South Korea", "South Korea"),
+    ("Kosovo", "Kosovo"),
+    ("Kuwait", "Kuwait"),
+    ("Kyrgyzstan", "Kyrgyzstan"),
+    ("Laos", "Laos"),
+    ("Latvia", "Latvia"),
+    ("Lebanon", "Lebanon"),
+    ("Lesotho", "Lesotho"),
+    ("Liberia", "Liberia"),
+    ("Libya", "Libya"),
+    ("Liechtenstein", "Liechtenstein"),
+    ("Lithuania", "Lithuania"),
+    ("Luxembourg", "Luxembourg"),
+    ("Macau", "Macau"),
+    ("Macedonia", "Macedonia"),
+    ("Madagascar", "Madagascar"),
+    ("Malawi", "Malawi"),
+    ("Malaysia", "Malaysia"),
+    ("Maldives", "Maldives"),
+    ("Mali", "Mali"),
+    ("Malta", "Malta"),
+    ("Marshall Islands", "Marshall Islands"),
+    ("Martinique", "Martinique"),
+    ("Mauritania", "Mauritania"),
+    ("Mauritius", "Mauritius"),
+    ("Mayotte", "Mayotte"),
+    ("Mexico", "Mexico"),
+    ("Micronesia", "Micronesia"),
+    ("Moldova", "Moldova"),
+    ("Monaco", "Monaco"),
+    ("Mongolia", "Mongolia"),
+    ("Montenegro", "Montenegro"),
+    ("Montserrat", "Montserrat"),
+    ("Morocco", "Morocco"),
+    ("Mozambique", "Mozambique"),
+    ("Myanmar", "Myanmar"),
+    ("Nagorno - Karabakh", "Nagorno - Karabakh"),
+    ("Namibia", "Namibia"),
+    ("Nauru", "Nauru"),
+    ("Nepal", "Nepal"),
+    ("Netherlands", "Netherlands"),
+    ("Netherlands Antilles", "Netherlands Antilles"),
+    ("New Caledonia", "New Caledonia"),
+    ("New Zealand", "New Zealand"),
+    ("Nicaragua", "Nicaragua"),
+    ("Niger", "Niger"),
+    ("Nigeria", "Nigeria"),
+    ("Niue", "Niue"),
+    ("Norfolk Island", "Norfolk Island"),
+    ("Turkish Republic of Northern Cyprus",
+        "Turkish Republic of Northern Cyprus"),
+    ("Northern Mariana", "Northern Mariana"),
+    ("Norway", "Norway"),
+    ("Oman", "Oman"),
+    ("Pakistan", "Pakistan"),
+    ("Palau", "Palau"),
+    ("Palestine", "Palestine"),
+    ("Panama", "Panama"),
+    ("Papua New Guinea", "Papua New Guinea"),
+    ("Paraguay", "Paraguay"),
+    ("Peru", "Peru"),
+    ("Philippines", "Philippines"),
+    ("Pitcairn Islands", "Pitcairn Islands"),
+    ("Poland", "Poland"),
+    ("Portugal", "Portugal"),
+    ("Puerto Rico", "Puerto Rico"),
+    ("Qatar", "Qatar"),
+    ("Romania", "Romania"),
+    ("Russia", "Russia"),
+    ("Rwanda", "Rwanda"),
+    ("Saint Barthelemy", "Saint Barthelemy"),
+    ("Saint Helena", "Saint Helena"),
+    ("Saint Kitts and Nevis", "Saint Kitts and Nevis"),
+    ("Saint Lucia", "Saint Lucia"),
+    ("Saint Martin", "Saint Martin"),
+    ("Saint Pierre and Miquelon", "Saint Pierre and Miquelon"),
+    ("Saint Vincent and the Grenadines",
+        "Saint Vincent and the Grenadines"),
+    ("Samoa", "Samoa"),
+    ("San Marino", "San Marino"),
+    ("Sao Tome and Principe", "Sao Tome and Principe"),
+    ("Saudi Arabia", "Saudi Arabia"),
+    ("Senegal", "Senegal"),
+    ("Serbia", "Serbia"),
+    ("Seychelles", "Seychelles"),
+    ("Sierra Leone", "Sierra Leone"),
+    ("Singapore", "Singapore"),
+    ("Slovakia", "Slovakia"),
+    ("Slovenia", "Slovenia"),
+    ("Solomon Islands", "Solomon Islands"),
+    ("Somalia", "Somalia"),
+    ("Somaliland", "Somaliland"),
+    ("South Africa", "South Africa"),
+    ("South Ossetia", "South Ossetia"),
+    ("Spain", "Spain"),
+    ("Sri Lanka", "Sri Lanka"),
+    ("Sudan", "Sudan"),
+    ("Suriname", "Suriname"),
+    ("Svalbard", "Svalbard"),
+    ("Swaziland", "Swaziland"),
+    ("Sweden", "Sweden"),
+    ("Switzerland", "Switzerland"),
+    ("Syria", "Syria"),
+    ("Taiwan", "Taiwan"),
+    ("Tajikistan", "Tajikistan"),
+    ("Tanzania", "Tanzania"),
+    ("Thailand", "Thailand"),
+    ("Timor - Leste", "Timor - Leste"),
+    ("Togo", "Togo"),
+    ("Tokelau", "Tokelau"),
+    ("Tonga", "Tonga"),
+    ("Transnistria Pridnestrovie", "Transnistria Pridnestrovie"),
+    ("Trinidad and Tobago", "Trinidad and Tobago"),
+    ("Tristan da Cunha", "Tristan da Cunha"),
+    ("Tunisia", "Tunisia"),
+    ("Turkey", "Turkey"),
+    ("Turkmenistan", "Turkmenistan"),
+    ("Turks and Caicos Islands", "Turks and Caicos Islands"),
+    ("Tuvalu", "Tuvalu"),
+    ("Uganda", "Uganda"),
+    ("Ukraine", "Ukraine"),
+    ("United Arab Emirates", "United Arab Emirates"),
+    ("United Kingdom", "United Kingdom"),
+    ("United States", "United States"),
+    ("Uruguay", "Uruguay"),
+    ("Uzbekistan", "Uzbekistan"),
+    ("Vanuatu", "Vanuatu"),
+    ("Vatican City", "Vatican City"),
+    ("Venezuela", "Venezuela"),
+    ("Vietnam", "Vietnam"),
+    ("British Virgin Islands", "British Virgin Islands"),
+    ("Isle of Man", "Isle of Man"),
+    ("US Virgin Islands", "US Virgin Islands"),
+    ("Wallis and Futuna", "Wallis and Futuna"),
+    ("Western Sahara", "Western Sahara"),
+    ("Yemen", "Yemen"),
+    ("Zambia", "Zambia"),
+    ("Zimbabwe", "Zimbabwe"),
+]
+
+STAFF_CHOICES = (
+    (
+        'Less than 10',
+        _('Less than 10')
+    ),
+    (
+        '10 to 50',
+        _('10 to 50')
+    ),
+    (
+        '51 to 250',
+        _('51 to 250')
+    ),
+    (
+        'More than 250',
+        _('More than 250')
+    ),
+)
+
+FEEDBACK_SERVICE = (
+    (
+        'Very satisfied',
+        _('Very satisfied')
+    ),
+    (
+        'Satisfied',
+        _('Satisfied')
+    ),
+    (
+        'Neither satisfied or dissatisfied',
+        _('Neither satisfied or dissatisfied', )
+    ),
+    (
+        'Dissatisfied',
+        _('Dissatisfied')
+    ),
+    (
+        'Very dissatisfied',
+        _('Very dissatisfied')
+    )
+)
 
 
-class LanguageForm(forms.Form):
-    lang = fields.ChoiceField(
-        choices=[]  # set by __init__
+class ContactForm(forms.Form):
+    def __init__(self, *args, **kwargs):
+        self.helper = FormHelper()
+        self.helper.layout = Layout(
+            Fieldset(
+                _("Contact Information"),
+                'name',
+                'job_title',
+                'email',
+                'phone_number',
+                css_class='form-group',
+            ),
+            Fieldset(
+                _("Company information"),
+                'company_name',
+                'company_website',
+                Field('country', id='js-country-select'),
+                'staff_number',
+                css_class='form-group',
+            ),
+            Fieldset(
+                _("Your plans"),
+                'description',
+                css_class='form-group',
+            ),
+            Div(
+                Field('captcha'),
+                HTML('<p class="form-label">{}</p>'.format(
+                    _(
+                        "By sending us your details you can confirm that "
+                        "the information you've shared with us is true "
+                        "and you accept our terms and conditions."
+                    )
+                )),
+                css_class='form-group',
+            ),
+            Submit("submit", _("Submit"), css_class='button button-blue')
+        )
+        super().__init__(*args, **kwargs)
+
+    name = fields.CharField(
+        label=_('Name'),
+        required=True)
+    job_title = fields.CharField(
+        label=_('Job title'),
+        required=True)
+    email = fields.EmailField(
+        label=_('Email address'),
+        required=True)
+    phone_number = fields.CharField(
+        label=_('Phone number'),
+        required=True)
+
+    company_name = fields.CharField(
+        label=_('Company name'),
+        required=True)
+    company_website = fields.CharField(
+        label=_('Website URL'),
+        required=False
+    )
+    country = fields.ChoiceField(
+        label=_('Which country are you based in?'),
+        help_text=_(
+            'We will use this information to put in touch with your '
+            'closest British embassy or high commission.'),
+        choices=BLANK_CHOICE_DASH+COUNTRIES_CHOICES
+
+    )
+    staff_number = fields.ChoiceField(
+        label=_('Current number of staff'),
+        choices=STAFF_CHOICES
+    )
+    description = fields.CharField(
+        label=_('Tell us about your investment'),
+        help_text=(
+            '<span class="form-hint">{}</span>'.format(
+                _('Tell us about your company and your plans for the UK in '
+                    'terms of size of investment, operational and recruitment '
+                    'plans. Please also tell us what help you would like from '
+                    'the UK government.'))),
+        widget=Textarea()
+    )
+    captcha = ReCaptchaField(
+        label='',
+        label_suffix='',
     )
 
-    def __init__(self, language_choices=settings.LANGUAGES, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.fields['lang'].choices = language_choices
 
-    def is_language_available(self, language_code):
-        language_codes = [code for code, _ in self.fields['lang'].choices]
-        return language_code in language_codes
+class FeedbackForm(forms.Form):
+    def __init__(self, *args, **kwargs):
+        self.helper = FormHelper()
+        self.helper.add_input(
+            Submit("submit", _("Submit"), css_class='btn btn_primary')
+        )
+        super().__init__(*args, **kwargs)
+
+    name = fields.CharField(label=_('Name'))
+    email = fields.EmailField(label=_('Email'))
+    service_quality = fields.ChoiceField(
+        label=_('How did you feel about the service you received today?'),
+        choices=FEEDBACK_SERVICE
+    )
+    feedback = fields.CharField(
+        label=_('How could we improve this service?'),
+        help_text=_(
+            "Please don't include any personal or financial information, "
+            "for example your National Insurance or credit card numbers."),
+        widget=Textarea
+    )
+    captcha = ReCaptchaField(
+        label='',
+        label_suffix='',
+    )
+
+
+class ReportIssueForm(forms.Form):
+    def __init__(self, *args, **kwargs):
+        self.helper = FormHelper()
+        self.helper.add_input(
+            Submit("submit", _("Submit"), css_class='btn btn_primary')
+        )
+        super().__init__(*args, **kwargs)
+
+    name = fields.CharField(label=_('Name'))
+    email = fields.EmailField(label=_('Email'))
+    feedback = fields.CharField(
+        label=_('Feedback'),
+        help_text=_('Maximum 1200 characters.'),
+        max_length=1200,
+        widget=Textarea,
+        validators=[no_html, not_contains_url_or_email]
+    )
+    captcha = ReCaptchaField(
+        label='',
+        label_suffix='',
+    )
+
+
+class SearchForm(forms.Form):
+
+    term = fields.CharField(
+        max_length=255,
+        required=False,
+    )
+    sectors = fields.ChoiceField(
+        required=False,
+        choices=(
+            (('', _('All industries')),) + choices.INDUSTRIES
+        ),
+        widget=Select(attrs={'class': 'bidi-rtl'})
+    )
