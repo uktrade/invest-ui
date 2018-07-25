4d2b458c30296380ed08e55b3d8b2b2bc112ddf6
eadpearce <e.pearce@tutanota.com>
2018-07-25 14:56:02 +0100

    Add tests

--- core/tests/test_templatetags.py
+++ core/tests/test_templatetags.py
@@ -1,5 +1,9 @@
 from django.template import Context, Template
 from django.utils import translation
+from core.templatetags.cms_tags import (
+    help_sections_to_list,
+    subsections_to_list,
+)
 
 from bs4 import BeautifulSoup
 
@@ -250,3 +254,86 @@ def test_filter_by_active_language(rf):
 
     assert len(soup.find_all('h1')) == 1
     assert soup.find('h1').string == 'Automotive'
+
+
+def test_how_we_help_sections_to_list():
+    page = {
+        'how_we_help_text_one': 'Text one',
+        'how_we_help_icon_one': {
+            'url': 'icon.png', 'width': 128, 'height': 128},
+        'how_we_help_text_two': 'Text two',
+        'how_we_help_icon_two': {
+            'url': 'icon.png', 'width': 128, 'height': 128},
+        'how_we_help_text_three': 'Text three',
+        'how_we_help_icon_three': {
+            'url': 'icon.png', 'width': 128, 'height': 128},
+        'how_we_help_text_four': 'Text four',
+        'how_we_help_icon_four': {
+            'url': 'icon.png', 'width': 128, 'height': 128},
+        'how_we_help_text_five': 'Text five',
+        'how_we_help_icon_five': {
+            'url': 'icon.png', 'width': 128, 'height': 128},
+        'how_we_help_text_six': 'Contact us',
+        }
+    exp = [
+        {
+            'text': 'Text one',
+            'icon': {'url': 'icon.png', 'width': 128, 'height': 128}
+        },
+        {
+            'text': 'Text two',
+            'icon': {'url': 'icon.png', 'width': 128, 'height': 128}
+        },
+        {
+            'text': 'Text three',
+            'icon': {'url': 'icon.png', 'width': 128, 'height': 128}
+        },
+        {
+            'text': 'Text four',
+            'icon': {'url': 'icon.png', 'width': 128, 'height': 128}
+        },
+        {
+            'text': 'Text five',
+            'icon': {'url': 'icon.png', 'width': 128, 'height': 128}
+        },
+        {'text': 'Contact us'},
+    ]
+    assert help_sections_to_list(page) == exp
+
+
+def test_subsections_to_list():
+    page = {
+        'subsection_title_one': 'Title 1',
+        'subsection_content_one': '<p>Content 1.</p>',
+        'subsection_map_one': None,
+        'subsection_title_two': 'Title 2',
+        'subsection_content_two': '<p>Content 2.</p>',
+        'subsection_map_two': {
+            'url': 'map.png', 'width': 1000, 'height': 1290},
+        'subsection_title_three': 'Title 3',
+        'subsection_content_three': '<p>Content 3.</p>',
+        'subsection_map_three': None,
+        'subsection_title_four': 'Title 4',
+        'subsection_content_four': '<p>Content 4.</p>',
+        'subsection_map_four': None,
+        'subsection_title_five': '',
+        'subsection_content_five': '',
+        'subsection_map_five': None,
+        'subsection_title_six': '',
+        'subsection_content_six': '',
+        'subsection_map_six': None,
+        'subsection_title_seven': '',
+        'subsection_content_seven': '',
+        'subsection_map_seven': None,
+        }
+    exp = [
+        {'content': '<p>Content 1.</p>', 'title': 'Title 1', 'map': None},
+        {'content': '<p>Content 2.</p>', 'title': 'Title 2',
+            'map': {'url': 'map.png', 'width': 1000, 'height': 1290}},
+        {'content': '<p>Content 3.</p>', 'title': 'Title 3', 'map': None},
+        {'content': '<p>Content 4.</p>', 'title': 'Title 4', 'map': None},
+        {'content': '', 'map': None, 'title': ''},
+        {'content': '', 'map': None, 'title': ''},
+        {'content': '', 'map': None, 'title': ''},
+    ]
+    assert subsections_to_list(page) == exp
